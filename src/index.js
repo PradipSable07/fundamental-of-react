2bc026d68e16c3f1708aeb5ea3da37e565c37dd3
Pradip Sable <sablepradipsubhash@gmail.com>
2023-10-04 22:52:25 +0530

    sxy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const books = [
+	{
+		BookName: "Atomic Habits",
+		AuthorName: "James Clear",
+		Image:
+			"https://m.media-amazon.com/images/I/91bYsX41DVL._AC_UY545_FMwebp_QL65_.jpg",
+		id: 1,
+	},
+	{
+		BookName: "The Midnight Library",
+		AuthorName: "Matt Haig",
+		Image:
+			"https://m.media-amazon.com/images/I/61QX1kV+MEL._AC_UY545_FMwebp_QL65_.jpg",
+		id: 2,
+	},
+	{
+		BookName: "How to Win Friends and Influence People",
+		AuthorName: " Dale Carnegie ",
+		Image:
+			"https://m.media-amazon.com/images/I/618XGVFYlwL._AC_UY545_FMwebp_QL65_.jpg",
+		id: 3,
+	},
+];
+const BookList = () => {
+	return (
+		<section className='booklist'>
+			{books.map((book) => {
+				return <Book {...book} key={book.id} />;
+			})}
+		</section>
+	);
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Book = (props) => {
+	const { Image, BookName, AuthorName } = props;
+	return (
+		<article className='book'>
+			<img src={Image} alt={BookName} />
+			<h2>{BookName}</h2>
+			<h4>{AuthorName}</h4>
+		</article>
+	);
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// handle event in react js
